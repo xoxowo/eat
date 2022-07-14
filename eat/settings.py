cdf87a189447facd57924f76a12d4def0b7bbbae
xoxowo <1qq11212@gmail.com>
2022-07-14 17:06:47 +0900

    test

--- eat/settings.py
+++ eat/settings.py
@@ -52,6 +52,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'eat.wsgi.application'
+
 DATABASES        = DATABASES
 
 AUTH_PASSWORD_VALIDATORS = [
@@ -99,3 +100,11 @@ CORS_ALLOW_HEADERS = (
     'x-csrftoken',
     'x-requested-with',
 )
+
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+    'django.contrib.auth.hashers.Argon2PasswordHasher',
+    'django.contrib.auth.hashers.ScryptPasswordHasher',
+]
--- users/views.py
+++ users/views.py
@@ -1,58 +1,65 @@
 import json
 import re
+import bcrypt
+import jwt
 
-
-from unittest     import result
 from django.http  import JsonResponse
-from django.core.exceptions import ObjectDoesNotExist
-
 from django.views import View
+
 from users.models import StoreUser
+from my_settings  import SECRET_KEY, ALGORITHM
 
 class SignUpView(View):
     def post(self, request):
         data = json.loads(request.body) 
-        email_form = re.compile('^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')
-        password_form = re.compile('^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$')
 
         try :
-            if data['password'] and data['email'] == None :
-                return JsonResponse({"message": "Email or Password is Empyt"}, status = 400)   
+            name         = data['name']
+            email        = data['email']
+            password     = data['password']
+            phone_number = data['phone_number']
 
-            if StoreUser.objects.filter(email=data['email']).exists():
-                return JsonResponse({"message": "Already registered Email"}, status = 400)   
-
-            if not email_form.match(data['email']) :
+            if not re.match('^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$', email) :
                 return JsonResponse({"message": "Invalid email format"}, status = 400)   
 
-            if not password_form.match(data['password']):
+            if not re.match('^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$', password):
                 return JsonResponse({"message": "Invalid password format"}, status = 400)
+
+            if StoreUser.objects.filter(email=email).exists():
+                return JsonResponse({"message": "Already registered Email"}, status = 400)   
             
+            hash_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')            
+
             StoreUser.objects.create(
-                name         = data['name'],
-                email        = data['email'],
-                password     = data['password'],
-                phone_number = data['phone_number'],
+                name          = name,
+                email         = email,
+                password      = hash_password,
+                phone_number  = phone_number,
             )
             return JsonResponse({"message": "SUCCESS"}, status = 201)
             
         except KeyError :
-            return JsonResponse({"message": "KEY_ERROR"}, status = 400)  
+            return JsonResponse({"message": "KEY_ERROR"}, status = 400)      
 
 class SignInView(View):
     def post(self, request):
         data = json.loads(request.body)
 
         try: 
-            if not StoreUser.objects.filter(email=data['email']).exists() :
-                return JsonResponse({"message": "INVALID_USER_NOT MATCH"}, status = 401)   
+            email    = data['email']
+            password = data['password']
 
-            if StoreUser.objects.filter(email=data['email']):
-   
-                if StoreUser.objects.filter(password = data['password']):
-                    return JsonResponse({"message": "SUCCESS"}, status = 200) 
-                else:
-                    return JsonResponse({"message": "INVALID_USER_password error"}, status = 401) 
+            user = StoreUser.objects.get(email=email) 
+               
+            if not bcrypt.checkpw(password.encode('utf-8'), user.password.encode('utf-8')):
+                return JsonResponse({"message": "INVALID_USER"}, status = 401)   
 
-        except KeyError :
+            access_token = jwt.encode({"id": user.id }, SECRET_KEY, ALGORITHM)
+            
+            return JsonResponse({"message": access_token }, status = 200)
+
+        except StoreUser.DoesNotExist:
+            return JsonResponse({"message": "INVALID_USER"}, status = 401)         
+
+        except KeyError:
             return JsonResponse({"message": "KEY_ERROR"}, status = 400)
